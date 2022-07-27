433820ae2c21fdb3cb9ff1cef5a4b5773e5b72b3
aasia27 <fatma.sia2020@gmail.com>
2022-07-27 19:55:30 +0530

    Updates

---
+++ precache-manifest.1cc4226d62471b7fcebee593a8c99373.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = [
+  {
+    "revision": "ac772f5980d16c6a20fc",
+    "url": "/Job-Hunter-React/static/css/main.32c2243a.chunk.css"
+  },
+  {
+    "revision": "ac772f5980d16c6a20fc",
+    "url": "/Job-Hunter-React/static/js/main.ac772f59.chunk.js"
+  },
+  {
+    "revision": "8f2f4211b0891106f089",
+    "url": "/Job-Hunter-React/static/js/1.8f2f4211.chunk.js"
+  },
+  {
+    "revision": "ed41a51636fd67a05eef",
+    "url": "/Job-Hunter-React/static/js/2.ed41a516.chunk.js"
+  },
+  {
+    "revision": "4cf5854ddacd9b212d6d",
+    "url": "/Job-Hunter-React/static/js/runtime~main.4cf5854d.js"
+  },
+  {
+    "revision": "b34edbefc21f2ce671baca9084593ad0",
+    "url": "/Job-Hunter-React/static/media/bgImg.b34edbef.jpg"
+  },
+  {
+    "revision": "b013bd2f318af2815c74072242c5befb",
+    "url": "/Job-Hunter-React/static/media/applyMain.b013bd2f.png"
+  },
+  {
+    "revision": "df3432b488f4fa735aa6016d25e82946",
+    "url": "/Job-Hunter-React/static/media/joinMain.df3432b4.png"
+  },
+  {
+    "revision": "2738af19fecc1e159164e34f85662b42",
+    "url": "/Job-Hunter-React/static/media/intro_pic1.2738af19.png"
+  },
+  {
+    "revision": "14e69bdd0346b5da183d38948b2ed5c0",
+    "url": "/Job-Hunter-React/static/media/main_2ndImage.14e69bdd.png"
+  },
+  {
+    "revision": "3645a8b52c64c733a8c8fe97ec84000d",
+    "url": "/Job-Hunter-React/static/media/About_img.3645a8b5.png"
+  },
+  {
+    "revision": "e4d3b895098718209107357f1def91ff",
+    "url": "/Job-Hunter-React/static/media/aasPic.e4d3b895.png"
+  },
+  {
+    "revision": "de7f873ac32767a604ff0189509ca439",
+    "url": "/Job-Hunter-React/static/media/tannuPic.de7f873a.png"
+  },
+  {
+    "revision": "28c4c00e0001def77daeb1c65bb71053",
+    "url": "/Job-Hunter-React/static/media/indus_login.28c4c00e.png"
+  },
+  {
+    "revision": "b5b651ab0ba7432d58e70a57cc4ac431",
+    "url": "/Job-Hunter-React/static/media/stud_login.b5b651ab.png"
+  },
+  {
+    "revision": "d242747193351d9ac0c03cf276291c62",
+    "url": "/Job-Hunter-React/static/media/contactUsBg.d2427471.png"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/Job-Hunter-React/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "72e486aeaf292aa41b0e73f743cfe9e6",
+    "url": "/Job-Hunter-React/static/media/recruiter_img.72e486ae.png"
+  },
+  {
+    "revision": "58366ccb2c3a69eed95a60f666bb43d5",
+    "url": "/Job-Hunter-React/index.html"
+  }
+];
